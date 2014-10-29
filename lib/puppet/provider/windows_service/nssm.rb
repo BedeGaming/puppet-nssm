5a9e839a416875a39531568a5e45810796c53079
Kurt Gardiner <k.a.gardiner@gmail.com>
2014-10-30 10:57:30 +1100

    Get instances working for the provider so puppet resource now works

--- lib/puppet/provider/windows_service/nssm.rb
+++ lib/puppet/provider/windows_service/nssm.rb
@@ -5,17 +5,10 @@ Puppet::Type.type(:windows_service).provide(:nssm) do
 
   commands :nssm => 'nssm', :powershell => 'powershell'
 
+  mk_resource_methods
+
   def exists?
     @property_hash[:ensure] == :present
-=begin
-    begin
-      output = nssm('status',resource[:name])
-    rescue Puppet::ExecutionFailure => e # Means a non zero exit code was returned
-      Puppet.debug("#nssm exists has an error -> #{e.inspect}")
-      return false
-    end
-    true
-=end
   end
 
   def create
@@ -25,49 +18,41 @@ Puppet::Type.type(:windows_service).provide(:nssm) do
 
   def destroy
     Puppet.debug("#nssm destroy the resource #{resource[:name]}!")
-    nssm(['remove',resource[:name]])
+    nssm('remove',resource[:name],'confirm')
   end 
 
+  # returns all providers for all existing services and startup state
   def self.instances
-    get_list_of_services.collect do |svc|
-      service_properties = get_service_property(service)
+    Win32::Service.services.collect do |svc|
+      service_properties = get_service_properties(svc)
       new(service_properties)
     end
   end
 
-  def self.get_list_of_services
-    services = powershell('Get-Service', '|select name|ConvertTo-Csv -NoTypeInformation').split('\n')
-    services.shift
-    services.sort
-  end
-
   def self.get_service_properties(service)
     service_properties = {}
- 
+    service_name = service.service_name
+
     begin
-       status = nssm('status', service)
-       parameters = nssm('get', service,'AppParameters')
-       start_in = nssm('get', service,'AppDirectory')
-       command = nssm('get', service,'Application')
-       #output = powershell('Get-Service', '-Name', service,' |select status,name,displayName|ConvertTo-Csv -NoTypeInformation').split('\n')
-       #services.shift
-       #services.sort
+      status = service.current_state
+      service_properties[:ensure] = :present
+      if service.binary_path_name.include? 'nssm'
+        parameters = nssm('get', service_name,'AppParameters').to_s.gsub("\x00",'').gsub("\n",'').gsub("\r",'')
+        start_in = nssm('get', service_name,'AppDirectory').to_s.gsub("\x00",'').gsub("\n",'').gsub("\r",'')
+        Puppet.debug("Got #{parameters.class} #{parameters.inspect} #{start_in}.")
+        command = nssm('get', service_name,'Application').to_s.gsub("\x00",'').gsub("\n",'').gsub("\r",'')
+      end
     rescue Puppet::ExecutionFailure => e
-      Puppet.debug("#nssm tried to run `nssm status` and the command returned non-zero. Assuming absent here...")
       service_properties[:ensure] = :absent
       #raise Puppet::Error, "#nssm tried to run `powershell Get-Service -Name #{service}|select status,name,displayName|ConvertTo-Csv -NoTypeInformation` and the command returned non-zero. Failing here..."
     end
- 
-    #service_properties[:ensure] = status == 'SERVICE_RUNNING' ? :present : :absent
-    service_properties[:ensure] = :present
+
     service_properties[:start_in] = start_in
     service_properties[:command] = command
     service_properties[:parameters] = parameters
 
     service_properties[:provider] = :nssm
-    service_properties[:name]     = service.downcase
+    service_properties[:name]     = service_name.downcase
     service_properties
-
   end
 end
-
