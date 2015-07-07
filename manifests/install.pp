977dc7581cc4d9f321279a45a22747f49d899ae4
Rob Rankin <github@undertow.ca>
2015-07-07 11:03:38 +0000

    test

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,9 +4,6 @@ class nssm::install {
 
   file { $nssm::install_dir:
     ensure => directory,
-  }->
-  file { $nssm::config_dir:
-    ensure => directory,
   }
 
   archive { "C:\\Windows\\Temp\\${nssm::archive_name}":
@@ -14,7 +11,7 @@ class nssm::install {
     extract      => true,
     extract_path => $nssm::install_dir,
     source       => "${nssm::archive_url}/${nssm::archive_name}",
-    creates      => "${nssm::install_dir}\\nssm.exe",
+    creates      => "${nssm::install_dir}",
     require      => File[$nssm::install_dir]
   }
 
