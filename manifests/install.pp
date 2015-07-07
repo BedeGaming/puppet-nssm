3b768f64771bf4d30a0035e56207eead256de7ea
Rob Rankin <github@undertow.ca>
2015-07-07 13:06:26 +0000

    remove program files dir

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,17 +2,12 @@ class nssm::install {
 
   include '::archive'
 
-  file { $nssm::install_dir:
-    ensure => directory,
-  }
-
   archive { "C:\\Windows\\Temp\\${nssm::archive_name}":
     ensure       => present,
     extract      => true,
     extract_path => 'C:\\Windows\\Temp\\',
     source       => "${nssm::archive_url}/${nssm::archive_name}",
     creates      => 'C:\\Windows\\Temp\\nssm-2.24',
-    require      => File[$nssm::install_dir]
   }
 
   file { 'C:\\Windows\\system32\\nssm.exe':
