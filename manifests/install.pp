ce493f2e08e4f0066d7cbfa6f29954573cbda5dc
Rob Rankin <github@undertow.ca>
2015-07-07 11:21:48 +0000

    test

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,7 +11,7 @@ class nssm::install {
     extract      => true,
     extract_path => $nssm::install_dir,
     source       => "${nssm::archive_url}/${nssm::archive_name}",
-    creates      => "${nssm::install_dir}",
+    #  creates      => "${nssm::install_dir}",
     require      => File[$nssm::install_dir]
   }
 
